6b46c42695a6b46d3b51e262a7840d0de3024e55
TerezaBediova <167741228+TerezaBediova@users.noreply.github.com>
2024-04-22 08:40:24 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <style class="css"></style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nadpis</title>
+</head>
+<body>
+    Tereza
+    <?php
+$den='sobota';
+$mesiac='april';
+
+  echo "<p> Dnes je $den, a mesiac je $mesiac.</p>";
+
+
+    ?>
+
+<br>
+
+</body>
+</html>
